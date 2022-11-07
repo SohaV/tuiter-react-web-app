34c4125cb8e802dd037e68bd5c0cb67b26ec27d2
SOHA-VANKUDRE\vanku <vankudresoha12@gmail.com>
2022-11-06 22:20:57 -0500

    A7

--- src/tuiter/profile/index.js
+++ src/tuiter/profile/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '@fortawesome/fontawesome-free/css/all.css';
-import {useDispatch,useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {useNavigate} from 'react-router-dom';
 
 const ProfileComponent = () => {
