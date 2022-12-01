53bf5e165a577af01c4ac8ff90954cd21e3deeb0
SOHA-VANKUDRE\vanku <vankudresoha12@gmail.com>
2022-12-01 18:36:53 -0500

    Explore changes

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 // import postsArray from './posts.json';
 import PostSummaryItem
     from "./post-summary-item.js";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {findTuitsThunk}
+    from "../../services/tuits-thunks.js";
 
 const PostSummaryList = () => {
-    const postsArray = useSelector(state => state.tuits)
+    // const postsArray = useSelector(state => state.tuits)
+    const {tuits, loading} = useSelector(state => state.tuitsData)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findTuitsThunk())
+        // eslint-disable-next-line
+    }, [])
     return(
         <ul className="list-group">
             {
-                postsArray.map(post =>
+                tuits.map(post =>
                     <PostSummaryItem
-                        key={postsArray._id} post={post}/> )
+                        key={tuits._id} post={post}/> )
             }
         </ul>
     );
